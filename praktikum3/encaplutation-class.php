cc91a1b2a247abbe7af46fa7b51ccf88d7e32d63
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-08 13:46:09 +0800

    error this protected and private

---
+++ praktikum3/encaplutation-class.php
@@ -0,0 +1,54 @@
+<?php
+
+class User
+{
+    public $firstName;
+    public $lasName;
+    public $username;
+
+    protected $regId = 1001;
+    private $level = "User";
+
+    function fullName()
+    {
+        return $this->firstName . " " . $this->lasName;
+    }
+
+    protected function sayProtected()
+    {
+        return "Hello protected";
+    }
+
+    private function sayPrivate()
+    {
+        echo "Hello private";
+    }
+}
+
+class Customer extends User
+{
+    public function sayParent()
+    {
+        return $this->sayProtected();
+    }
+}
+
+$u = new User;
+$u->firstName = "User";
+$u->lasName = "Class";
+
+// echo $u->regId . '<br>';
+// echo $u->level . '<br>';
+
+echo $u->fullName() . '<br>';
+// echo $u->sayProtected() . '<br>';
+// echo $u->sayPrivate() . '<br>';
+
+
+
+$c = new Customer;
+$c->firstName = "Rafi";
+$c->lasName = "Akmal";
+
+echo $c->fullName()  . "<br>";
+echo $c->sayParent() . "<br>";
