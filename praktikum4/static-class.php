a665861aaa910d778642498b64b06a88bfa48cd3
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-10 01:39:32 +0800

    feat : add static-class

---
+++ praktikum4/static-class.php
@@ -0,0 +1,30 @@
+<?php
+
+class Student
+{
+    public static $grades = ["SD", "SMP", "SMA"];
+    private static $totalStudent = 0;
+
+    public static function motto()
+    {
+        return "Learn PHP OOP";
+    }
+
+    public static function count()
+    {
+        return self::$totalStudent;
+    }
+
+    public static function addStudent()
+    {
+        return self::$totalStudent;
+    }
+}
+
+
+echo Student::$grades[2] . "<br>";
+echo Student::motto() . "<br>";
+
+echo Student::count() . "<br>";
+Student::addStudent();
+echo Student::count() . "<br>";
