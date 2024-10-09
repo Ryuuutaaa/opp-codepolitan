77a7a9a6f0a7cc2a688eea297bce5ab11ff4136d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-10 01:27:31 +0800

    feat : add getter-setter php

---
+++ praktikum3/getter-setter.php
@@ -0,0 +1,24 @@
+<?php
+
+class Product
+{
+    private $price;
+
+    public function setPrice($value)
+    {
+        $noFormat = preg_replace("/Rp/ ", "", $value);
+        $float = floatval($noFormat);
+
+        if ($float <= 0) {
+            // show error this
+            return;
+        }
+
+        $this->price = $float;
+    }
+
+    public function getPrice()
+    {
+        return "Rp" . number_format($this->price, 2);
+    }
+}
